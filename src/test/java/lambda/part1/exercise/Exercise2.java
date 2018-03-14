21aad711f5c0928d764c15c3dc287ed114bbdf1a
mercenery@inbox.ru <mercenery@inbox.ru>
2018-03-14 11:55:25 +0300

    exercise 2 done(Fedulov)

--- src/test/java/lambda/part1/exercise/Exercise2.java
+++ src/test/java/lambda/part1/exercise/Exercise2.java
@@ -6,82 +6,99 @@ import java.util.StringJoiner;
 
 import static org.junit.Assert.assertEquals;
 
-@SuppressWarnings({"FieldCanBeLocal", "ConstantConditions", "unused"})
 public class Exercise2 {
-
-    @FunctionalInterface
-    private interface Multiplier<T> {
-        T multiply(T value, int multiplier);
-
-        default T twice(T t) {
-            return multiply(t, 2);
-        }
-    }
-
-    private void testIntegerMultiplier(Multiplier<Integer> multiplier) {
-        assertEquals(6, multiplier.multiply(3, 2).intValue());
-        assertEquals(0, multiplier.multiply(Integer.MIN_VALUE, 0).intValue());
-        assertEquals(-7, multiplier.multiply(7, -1).intValue());
-        assertEquals(10, multiplier.twice(5).intValue());
-        assertEquals(0, multiplier.twice(0).intValue());
-    }
-
-    @Test
-    public void implementsIntegerMultiplierUsingAnonymousClass() {
-        Multiplier<Integer> multiplier = null;
-
-        testIntegerMultiplier(multiplier);
-    }
-
-    @Test
-    public void implementsMultiplierUsingStatementLambda() {
-        Multiplier<Integer> multiplier = null;
-
-        testIntegerMultiplier(multiplier);    }
-
-    @Test
-    public void implementsIntegerMultiplierUsingExpressionLambda() {
-        Multiplier<Integer> multiplier = null;
-
-        testIntegerMultiplier(multiplier);
-    }
-
-    private static String multiplyString(String string, int number) {
-        StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < number; ++i) {
-            builder.append(string);
-        }
-        return builder.toString();
-    }
-
-    @Test
-    public void implementsStringMultiplierUsingClassMethodReference() {
-        Multiplier<String> multiplier = null;
-
-        assertEquals("aaa", multiplier.multiply("a", 3));
-        assertEquals("", multiplier.multiply("qwerty", 0));
-        assertEquals("aAaA", multiplier.twice("aA"));
-        assertEquals("", multiplier.twice(""));
-    }
-
-    private final String delimiter = "-";
-
-    private String stringSumWithDelimiter(String string, int number) {
-        StringJoiner joiner = new StringJoiner(delimiter);
-        for (int i = 0; i < number; ++i) {
-            joiner.add(string);
-        }
-        String result = joiner.toString();
-        return result.equals(delimiter) ? "" : result;
-    }
-
-    @Test
-    public void implementsStringMultiplierUsingObjectMethodReference() {
-        Multiplier<String> multiplier = null;
-
-        assertEquals("a-a-a", multiplier.multiply("a", 3));
-        assertEquals("", multiplier.multiply("qwerty", 0));
-        assertEquals("A-A", multiplier.twice("A"));
-        assertEquals("", multiplier.twice(""));
-    }
+	
+	private final String delimiter = "-";
+	
+	private static String multiplyString(String string, int number) {
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < number; ++i) {
+			builder.append(string);
+		}
+		return builder.toString();
+	}
+	
+	private void testStringMultiplier(Multiplier<String> multiplier) {
+		objectMethodReferenceStringMultiplierAsserts(multiplier, "aaa", "aAaA", "aA");
+	}
+	
+	private void testIntegerMultiplier(Multiplier<Integer> multiplier) {
+		assertEquals(6, multiplier.multiply(3, 2).intValue());
+		assertEquals(0, multiplier.multiply(Integer.MIN_VALUE, 0).intValue());
+		assertEquals(-7, multiplier.multiply(7, -1).intValue());
+		assertEquals(10, multiplier.twice(5).intValue());
+		assertEquals(0, multiplier.twice(0).intValue());
+	}
+	
+	@Test
+	public void implementsIntegerMultiplierUsingAnonymousClass() {
+		Multiplier<Integer> multiplier = new Multiplier<Integer>() {
+			@Override
+			public Integer multiply(Integer value, int multiplier) {
+				return value * multiplier;
+			}
+		};
+		
+		testIntegerMultiplier(multiplier);
+	}
+	
+	@Test
+	public void implementsMultiplierUsingStatementLambda() {
+		Multiplier<Integer> multiplier = (Integer value, int multiplierr) -> {
+			return value * multiplierr;
+		};
+		
+		testIntegerMultiplier(multiplier);
+	}
+	
+	@Test
+	public void implementsIntegerMultiplierUsingExpressionLambda() {
+		Multiplier<Integer> multiplier = (Integer value, int multiplierr) -> value * multiplierr;
+		testIntegerMultiplier(multiplier);
+	}
+	
+	@Test
+	public void implementsStringMultiplierUsingClassMethodReference() {
+		Multiplier<String> multiplier = Exercise2::multiplyString;
+		testStringMultiplier(multiplier);
+	}
+	
+	private String stringSumWithDelimiter(String string, int number) {
+		StringJoiner joiner = new StringJoiner(delimiter);
+		for (int i = 0; i < number; ++i) {
+			joiner.add(string);
+		}
+		String result = joiner.toString();
+		return result.equals(delimiter) ? "" : result;
+	}
+	
+	@Test
+	public void implementsStringMultiplierUsingObjectMethodReference() {
+		Multiplier<String> multiplier =
+				this::multiplyStringNotStaticForObjectMethodReferenceWithDelimiter;
+		
+		objectMethodReferenceStringMultiplierAsserts(multiplier, "a-a-a", "A-A", "A");
+	}
+	
+	private String multiplyStringNotStaticForObjectMethodReferenceWithDelimiter(String string,
+																				int i) {
+		return stringSumWithDelimiter(string, i);
+	}
+	
+	private void objectMethodReferenceStringMultiplierAsserts(Multiplier<String> multiplier,
+															  String s, String s2, String a) {
+		assertEquals(s, multiplier.multiply("a", 3));
+		assertEquals("", multiplier.multiply("qwerty", 0));
+		assertEquals(s2, multiplier.twice(a));
+		assertEquals("", multiplier.twice(""));
+	}
+	
+	@FunctionalInterface
+	private interface Multiplier<T> {
+		T multiply(T value, int multiplier);
+		
+		default T twice(T t) {
+			return multiply(t, 2);
+		}
+	}
 }
