785f1568dca2b0fd9b1ebd34f491afafaa6bc7e7
wladez <wladez95@gmail.com>
2018-03-15 23:07:45 +0300

    Done Exercise3 from lambda part1

--- src/test/java/lambda/part1/exercise/Exercise3.java
+++ src/test/java/lambda/part1/exercise/Exercise3.java
@@ -1,10 +1,13 @@
 package lambda.part1.exercise;
 
+import com.google.common.collect.FluentIterable;
 import lambda.data.Person;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import com.google.common.base.Optional;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
@@ -17,6 +20,7 @@ public class Exercise3 {
         Person[] persons = getPersons();
 
         // TODO использовать Arrays.sort + expression-lambda
+        Arrays.sort(persons, (person1, person2) -> Integer.compare(person1.getAge(), person2.getAge()));
 
         assertArrayEquals(new Person[]{
             new Person("Иван", "Мельников", 20),
@@ -30,7 +34,10 @@ public class Exercise3 {
     public void sortPersonsByLastNameThenFirstNameUsingArraysSortExpressionLambda() {
         Person[] persons = getPersons();
 
+        Comparator<Person> comparator = (person1, person2) -> person1.getLastName().compareTo(person2.getLastName());
+
         // TODO использовать Arrays.sort + statement-lambda
+        Arrays.sort(persons, comparator.thenComparing((person1, person2) -> person1.getFirstName().compareTo(person2.getFirstName())));
 
         assertArrayEquals(new Person[]{
             new Person("Алексей", "Доренко", 40),
@@ -46,6 +53,10 @@ public class Exercise3 {
 
         // TODO использовать FluentIterable
         Person person = null;
+
+        Optional<Person> optionalPerson = FluentIterable.from(persons).firstMatch(person1 -> person1.getAge()==30);
+
+        person = optionalPerson.get();
 
         assertEquals(new Person("Николай", "Зимов", 30), person);
     }
