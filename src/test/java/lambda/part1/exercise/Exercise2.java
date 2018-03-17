df3bd9fe858db9a4402eb7113d55f890317e2f2d
Andrei <Andrei_Bogus@epam.com>
2018-03-17 15:15:54 +0300

    Revert "done"
    This reverts commit 2afeef325aeb369e8c06fcae9234ac2f137e6834.

--- src/test/java/lambda/part1/exercise/Exercise2.java
+++ src/test/java/lambda/part1/exercise/Exercise2.java
@@ -28,25 +28,21 @@ public class Exercise2 {
 
     @Test
     public void implementsIntegerMultiplierUsingAnonymousClass() {
-        Multiplier<Integer> multiplier = new Multiplier<Integer>() {
-            @Override
-            public Integer multiply(Integer value, int multiplier) {
-                                return value*multiplier;
-                            }
-        };
+        Multiplier<Integer> multiplier = null;
+
         testIntegerMultiplier(multiplier);
     }
 
     @Test
     public void implementsMultiplierUsingStatementLambda() {
-        Multiplier<Integer> multiplier = (value, mult) -> {
-                        return value*mult;
-                    };
+        Multiplier<Integer> multiplier = null;
+
         testIntegerMultiplier(multiplier);    }
 
     @Test
     public void implementsIntegerMultiplierUsingExpressionLambda() {
-        Multiplier<Integer> multiplier = (value,mult)-> value*mult;
+        Multiplier<Integer> multiplier = null;
+
         testIntegerMultiplier(multiplier);
     }
 
@@ -60,7 +56,7 @@ public class Exercise2 {
 
     @Test
     public void implementsStringMultiplierUsingClassMethodReference() {
-        Multiplier<String> multiplier = Exercise2::multiplyString;
+        Multiplier<String> multiplier = null;
 
         assertEquals("aaa", multiplier.multiply("a", 3));
         assertEquals("", multiplier.multiply("qwerty", 0));
@@ -81,7 +77,7 @@ public class Exercise2 {
 
     @Test
     public void implementsStringMultiplierUsingObjectMethodReference() {
-        Multiplier<String> multiplier = this::stringSumWithDelimiter;
+        Multiplier<String> multiplier = null;
 
         assertEquals("a-a-a", multiplier.multiply("a", 3));
         assertEquals("", multiplier.multiply("qwerty", 0));
