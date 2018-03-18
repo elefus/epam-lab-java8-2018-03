853ca555df8c3a4e5957aa43bd432d4613a4a426
Daryya Buchok <daria_buchok@epam.com>
2018-03-18 22:07:45 +0300

    part3.exercise4 - done.

--- src/test/java/lambda/part3/exercise/Exercise4.java
+++ src/test/java/lambda/part3/exercise/Exercise4.java
@@ -1,10 +1,12 @@
 package lambda.part3.exercise;
 
 import lambda.data.Employee;
+import lambda.data.JobHistoryEntry;
 import lambda.part3.example.Example1;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
@@ -15,20 +17,43 @@ public class Exercise4 {
 
     private static class LazyCollectionHelper<T, R> {
 
+        private final List<T> source;
+        private final Function<T, List<R>> function;
+
+        public LazyCollectionHelper(List<T> source, Function<T, List<R>> function) {
+            this.source = source;
+            this.function = function;
+        }
+
         public static <T> LazyCollectionHelper<T, T> from(List<T> list) {
-            throw new UnsupportedOperationException();
+            return new LazyCollectionHelper<>(list, Collections::singletonList);
         }
 
         public <U> LazyCollectionHelper<T, U> flatMap(Function<R, List<U>> flatMapping) {
-            throw new UnsupportedOperationException();
+            return new LazyCollectionHelper<>(source, function.andThen(getListListFunction(flatMapping)));
+        }
+
+        private <U> Function<List<R>, List<U>> getListListFunction(Function<R, List<U>> flatMapping) {
+            return rs -> {
+                List<U> result = new ArrayList<>();
+                for (R r : rs) {
+                    result.addAll(flatMapping.apply(r));
+                }
+                return result;
+            };
         }
 
         public <U> LazyCollectionHelper<T, U> map(Function<R, U> mapping) {
-            throw new UnsupportedOperationException();
+            return new LazyCollectionHelper<>(source,
+                    function.andThen(getListListFunction(mapping.andThen(Collections::singletonList))));
         }
 
         public List<R> force() {
-            throw new UnsupportedOperationException();
+            List<R> result = new ArrayList<>();
+            for (T t : source) {
+                result.addAll(function.apply(t));
+            }
+            return result;
         }
     }
 
@@ -36,17 +61,30 @@ public class Exercise4 {
     public void mapEmployeesToCodesOfLetterTheirPositionsUsingLazyFlatMapHelper() {
         List<Employee> employees = Example1.getEmployees();
 
-        List<Integer> codes = null;
-        // TODO              LazyCollectionHelper.from(employees)
-        // TODO                                  .flatMap(Employee -> JobHistoryEntry)
-        // TODO                                  .map(JobHistoryEntry -> String(position))
-        // TODO                                  .flatMap(String -> Character(letter))
-        // TODO                                  .map(Character -> Integer(code letter)
-        // TODO                                  .force();
+        Function<String, List<Character>> strToCharList = s -> {
+            List<Character> chars = new ArrayList<>();
+            for (char c : s.toCharArray()) {
+                chars.add(c);
+            }
+            return chars;
+        };
+
+        List<Integer> codes = LazyCollectionHelper.from(employees)
+                                                  .flatMap(Employee::getJobHistory)
+                                                  .map(JobHistoryEntry::getPosition)
+                                                  .flatMap(s -> {
+                                                      List<Character> chars = new ArrayList<>();
+                                                      for (char c : s.toCharArray()) {
+                                                          chars.add(c);
+                                                      }
+                                                      return chars;
+                                                  })
+                                                  .map(ch -> (int) ch)
+                                                  .force();
         assertEquals(calcCodes("dev", "dev", "tester", "dev", "dev", "QA", "QA", "dev", "tester", "tester", "QA", "QA", "QA", "dev"), codes);
     }
 
-    private static List<Integer> calcCodes(String...strings) {
+    private static List<Integer> calcCodes(String... strings) {
         List<Integer> codes = new ArrayList<>();
         for (String string : strings) {
             for (char letter : string.toCharArray()) {
