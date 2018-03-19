daed71de69a059e84d1b89f948516753fb1f3bdb
mercenery@inbox.ru <mercenery@inbox.ru>
2018-03-19 04:45:04 +0300

    lambda.part3.Exercise4.precomplete(Fedulov)

--- src/test/java/lambda/part3/exercise/Exercise4.java
+++ src/test/java/lambda/part3/exercise/Exercise4.java
@@ -5,30 +5,66 @@ import lambda.part3.example.Example1;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
 import static org.junit.Assert.assertEquals;
 
-@SuppressWarnings({"unused", "ConstantConditions"})
 public class Exercise4 {
 
     private static class LazyCollectionHelper<T, R> {
+        private List<T> source;
+        private Function<T, List<R>> remapping;
+
+
+        public LazyCollectionHelper(List<T> source, Function<T, List<R>> remapping) {
+            this.source = source;
+            this.remapping = remapping;
+        }
 
         public static <T> LazyCollectionHelper<T, T> from(List<T> list) {
-            throw new UnsupportedOperationException();
+//            throw new UnsupportedOperationException();
+
+            return new LazyCollectionHelper<>(list, Collections::singletonList);
         }
 
         public <U> LazyCollectionHelper<T, U> flatMap(Function<R, List<U>> flatMapping) {
-            throw new UnsupportedOperationException();
+//            throw new UnsupportedOperationException();
+            return new LazyCollectionHelper<>(source, remapping.andThen(transformUsing(flatMapping)));
+
         }
 
         public <U> LazyCollectionHelper<T, U> map(Function<R, U> mapping) {
-            throw new UnsupportedOperationException();
+//            throw new UnsupportedOperationException();
+            return new LazyCollectionHelper<>(source,
+                                              remapping.andThen(transformUsing(mapping.andThen
+                                                      (Collections::singletonList))));
+
         }
 
         public List<R> force() {
-            throw new UnsupportedOperationException();
+//            throw new UnsupportedOperationException();
+//            for (T t : source) {
+//                List<R> preResult = remapping.apply(t);
+//                result.addAll(preResult);
+//            }
+//            List<R> result = new ArrayList<>();
+//
+//            source.forEach(remapping.andThen(result::addAll)::apply);
+//
+//            return result;
+            return transformUsing(remapping).apply(source);
+        }
+
+        private <FROM, TO> Function<List<FROM>, List<TO>> transformUsing(Function<FROM, List<TO>> mapper) {
+            return source -> {
+                List<TO> result = new ArrayList<>();
+
+                source.forEach(mapper.andThen(result::addAll)::apply);
+
+                return result;
+            };
         }
     }
 
