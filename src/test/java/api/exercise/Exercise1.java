d79d1d49183244bf2909668a1a21cbdccfc414e7
wladez <wladez95@gmail.com>
2018-03-20 23:33:06 +0300

    Done Exercise1 from api

--- src/test/java/api/exercise/Exercise1.java
+++ src/test/java/api/exercise/Exercise1.java
@@ -31,6 +31,7 @@ public class Exercise1 {
         candidates.put(helen, Status.PENDING);
 
         // TODO реализация
+        candidates.replaceAll((person, status) -> person.getAge() > 21 ? Status.ACCEPTED : Status.DECLINED);
 
         assertEquals(Status.ACCEPTED, candidates.get(ivan));
         assertEquals(Status.ACCEPTED, candidates.get(helen));
@@ -52,6 +53,8 @@ public class Exercise1 {
         candidates.put(new Person("b", "c", 5), Status.PENDING);
 
         // TODO реализация
+        candidates.keySet().removeIf(person -> person.getAge() < 21);
+        candidates.replaceAll(((person, status) -> Status.ACCEPTED));
 
         Map<Person, Status> expected = new HashMap<>();
         expected.put(ivan, Status.ACCEPTED);
@@ -69,9 +72,9 @@ public class Exercise1 {
         candidates.put(ivan, Status.PENDING);
 
         // TODO реализация
-        Status alexStatus = null;
-        Status ivanStatus = null;
-        Status helenStatus = null;
+        Status alexStatus = candidates.getOrDefault(alex, Status.UNKNOWN);
+        Status ivanStatus = candidates.getOrDefault(ivan, Status.UNKNOWN);;
+        Status helenStatus = candidates.getOrDefault(helen, Status.UNKNOWN);;
 
         assertEquals(Status.PENDING, alexStatus);
         assertEquals(Status.PENDING, ivanStatus);
@@ -94,6 +97,7 @@ public class Exercise1 {
         newValues.put(helen, Status.PENDING);
 
         // TODO реализация
+        oldValues.forEach(newValues::putIfAbsent);
 
         assertEquals(Status.DECLINED, newValues.get(alex));
         assertEquals(Status.ACCEPTED, newValues.get(ivan));
