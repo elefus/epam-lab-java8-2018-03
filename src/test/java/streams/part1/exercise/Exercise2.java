769d0db6a336f494a32695761a75ddd11f6e6a0c
elefus <elefus@yandex.ru>
2018-03-20 15:52:46 +0300

    Код с занятия 19.03.2018

---
+++ src/test/java/streams/part1/exercise/Exercise2.java
@@ -0,0 +1,55 @@
+package streams.part1.exercise;
+
+import lambda.data.Employee;
+import lambda.data.Person;
+import lambda.part3.example.Example1;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@SuppressWarnings({"ConstantConditions", "unused"})
+public class Exercise2 {
+
+    @Test
+    public void calcAverageAgeOfEmployees() {
+        List<Employee> employees = Example1.getEmployees();
+
+        Double expected = null;
+
+        assertEquals(33.66, expected, 0.1);
+    }
+
+    @Test
+    public void findPersonWithLongestFullName() {
+        List<Employee> employees = Example1.getEmployees();
+
+        Person expected = null;
+
+        assertEquals(expected, employees.get(1).getPerson());
+    }
+
+    @Test
+    public void findEmployeeWithMaximumDurationAtOnePosition() {
+        List<Employee> employees = Example1.getEmployees();
+
+        Employee expected = null;
+
+        assertEquals(expected, employees.get(4));
+    }
+
+    /**
+     * Вычислить общую сумму заработной платы для сотрудников.
+     * Базовая ставка каждого сотрудника составляет 75_000.
+     * Если на текущей позиции (последняя в списке) он работает больше трех лет - ставка увеличивается на 20%
+     */
+    @Test
+    public void calcTotalSalaryWithCoefficientWorkExperience() {
+        List<Employee> employees = Example1.getEmployees();
+
+        Double expected = null;
+
+        assertEquals(465000.0, expected, 0.001);
+    }
+}
