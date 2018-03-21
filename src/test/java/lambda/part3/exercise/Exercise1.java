e1b23dcd9a2f420a8b2a0c265b273d2ceeae1539
Igor_Erofeevskiy <Igor_Erofeevskiy@epam.com>
2018-03-21 19:25:08 +0300

    lambda.part3.exercise.Exercise1 done

--- src/test/java/lambda/part3/exercise/Exercise1.java
+++ src/test/java/lambda/part3/exercise/Exercise1.java
@@ -1,12 +1,14 @@
 package lambda.part3.exercise;
 
 import lambda.data.Employee;
+import lambda.data.Person;
 import lambda.part3.example.Example1;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,11 +21,22 @@ public class Exercise1 {
         List<Integer> lengths = new ArrayList<>();
 
         // TODO функция извлечения информации о человеке из объекта сотрудника personExtractor: Employee -> Person
+        Function<Employee, Person> personExtractor = Employee::getPerson;
         // TODO функция извлечения полного имени из информации о человеке fullNameExtractor: Person -> String
+        Function<Person, String> fullNameExtractor = Person::getFullName;
         // TODO функция извлечения длины из строки stringLengthExtractor: String -> Integer
+        Function<String, Integer> stringLengthFunction = String::length;
         // TODO функция извлечения длины полного имени из сотрудника fullNameLengthExtractor: Employee -> Integer
+        Function<Employee, Integer> fullNameLengthExtractor = personExtractor.andThen(fullNameExtractor).andThen(stringLengthFunction);
+        //Function<Employee, Integer> fullNameLengthExtractor = employee -> employee.getPerson().getFullName().length();
         // TODO преобразование списка employees в lengths используя fullNameLengthExtractor
+//        for (Employee employee : employees) {
+//            lengths.add(fullNameLengthExtractor.apply(employee));
+//        }
 
+//      employees.forEach(employee -> lengths.add(fullNameLengthExtractor.apply(employee)));
+
+        employees.forEach(fullNameLengthExtractor.andThen(lengths::add)::apply);
         assertEquals(Arrays.asList(14, 19, 14, 15, 14, 16), lengths);
     }
 }
