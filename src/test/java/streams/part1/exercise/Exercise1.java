562c1e566720ab5e46194446bd6768885f76b09c
mercenery@inbox.ru <mercenery@inbox.ru>
2018-03-21 17:34:02 +0300

    streams.part1.exercises.Exercise1.butLast(Fedulov)

--- src/test/java/streams/part1/exercise/Exercise1.java
+++ src/test/java/streams/part1/exercise/Exercise1.java
@@ -1,18 +1,20 @@
 package streams.part1.exercise;
 
 import lambda.data.Employee;
+import lambda.data.JobHistoryEntry;
 import lambda.data.Person;
 import lambda.part3.example.Example1;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
-@SuppressWarnings({"ConstantConditions", "unused"})
 public class Exercise1 {
 
     @Test
@@ -20,13 +22,21 @@ public class Exercise1 {
         List<Employee> employees = Example1.getEmployees();
 
         // TODO реализация, использовать Collectors.toList()
-        List<Person> personsEverWorkedInEpam = null;
-
-        List<Person> expected = Arrays.asList(
-                employees.get(0).getPerson(),
-                employees.get(1).getPerson(),
-                employees.get(4).getPerson(),
-                employees.get(5).getPerson());
+        List<Person>
+                personsEverWorkedInEpam =
+                employees.stream()
+                         .filter(employee -> employee.getJobHistory()
+                                                     .stream()
+                                                     .map(JobHistoryEntry::getEmployer)
+                                                     .anyMatch("EPAM"::equalsIgnoreCase))
+                         .map(Employee::getPerson)
+                         .collect(Collectors.toList());
+        List<Person>
+                expected =
+                Arrays.asList(employees.get(0).getPerson(),
+                              employees.get(1).getPerson(),
+                              employees.get(4).getPerson(),
+                              employees.get(5).getPerson());
         assertEquals(expected, personsEverWorkedInEpam);
     }
 
@@ -35,12 +45,16 @@ public class Exercise1 {
         List<Employee> employees = Example1.getEmployees();
 
         // TODO реализация, использовать Collectors.toList()
-        List<Person> startedFromEpam = null;
+        List<Person>
+                startedFromEpam =
+                employees.stream()
+                         .filter(employee -> employee.getJobHistory().get(0).equals("EPAM"))
+                         .map(Employee::getPerson)
+                         .collect(Collectors.toList());
 
-        List<Person> expected = Arrays.asList(
-                employees.get(0).getPerson(),
-                employees.get(1).getPerson(),
-                employees.get(4).getPerson());
+        List<Person>
+                expected =
+                Arrays.asList(employees.get(0).getPerson(), employees.get(1).getPerson(), employees.get(4).getPerson());
         assertEquals(expected, startedFromEpam);
     }
 
@@ -49,7 +63,12 @@ public class Exercise1 {
         List<Employee> employees = Example1.getEmployees();
 
         // TODO реализация, использовать Collectors.toSet()
-        Set<String> companies = null;
+        Set<String>
+                companies =
+                employees.stream()
+                         .flatMap(employee -> employee.getJobHistory().stream())
+                         .map(JobHistoryEntry::getEmployer)
+                         .collect(Collectors.toSet());
 
         Set<String> expected = new HashSet<>();
         expected.add("EPAM");
@@ -61,11 +80,15 @@ public class Exercise1 {
     }
 
     @Test
-    public void findMinimalAgeOfEmployees() {
+    public void findMinimalAgeOfEmployees() throws Exception {
         List<Employee> employees = Example1.getEmployees();
 
         // TODO реализация
-        Integer minimalAge = null;
+        Integer
+                minimalAge =
+                employees.stream().map(Employee::getPerson).map(Person::getAge).min(Comparator.naturalOrder()).get();
+        minimalAge =
+                employees.stream().map(Employee::getPerson).mapToInt(Person::getAge).min().orElseThrow(Exception::new);
 
         assertEquals(21, minimalAge.intValue());
     }
