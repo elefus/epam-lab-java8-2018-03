52d0ce676f6b978a75737e842c10dc8b9c2fb059
Viacheslav Plekhanov <Viacheslav_PLekhanov@epam.com>
2018-03-22 23:41:17 +0300

    api.exercise.Exercise1(Plekhanov)

--- src/test/java/api/exercise/Exercise1.java
+++ src/test/java/api/exercise/Exercise1.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.function.Function;
 
 import static org.junit.Assert.assertEquals;
 
@@ -30,7 +31,9 @@ public class Exercise1 {
         candidates.put(ivan, Status.PENDING);
         candidates.put(helen, Status.PENDING);
 
-        // TODO реализация
+        candidates.replaceAll((person, status)-> person.getAge()>21
+                ?Status.ACCEPTED
+                :Status.DECLINED);
 
         assertEquals(Status.ACCEPTED, candidates.get(ivan));
         assertEquals(Status.ACCEPTED, candidates.get(helen));
@@ -51,7 +54,8 @@ public class Exercise1 {
         candidates.put(new Person("b", "c", 16), Status.PENDING);
         candidates.put(new Person("b", "c", 5), Status.PENDING);
 
-        // TODO реализация
+        candidates.keySet().removeIf((person)->person.getAge()<=21);
+        candidates.replaceAll(((person, status) -> Status.ACCEPTED));
 
         Map<Person, Status> expected = new HashMap<>();
         expected.put(ivan, Status.ACCEPTED);
@@ -69,9 +73,10 @@ public class Exercise1 {
         candidates.put(ivan, Status.PENDING);
 
         // TODO реализация
-        Status alexStatus = null;
-        Status ivanStatus = null;
-        Status helenStatus = null;
+        Function<Person,Status> getStatus= person -> candidates.getOrDefault(person, Status.UNKNOWN);
+        Status alexStatus = getStatus.apply(alex);
+        Status ivanStatus = getStatus.apply(ivan);
+        Status helenStatus = getStatus.apply(helen);
 
         assertEquals(Status.PENDING, alexStatus);
         assertEquals(Status.PENDING, ivanStatus);
@@ -94,6 +99,7 @@ public class Exercise1 {
         newValues.put(helen, Status.PENDING);
 
         // TODO реализация
+        oldValues.forEach(newValues::putIfAbsent);
 
         assertEquals(Status.DECLINED, newValues.get(alex));
         assertEquals(Status.ACCEPTED, newValues.get(ivan));
