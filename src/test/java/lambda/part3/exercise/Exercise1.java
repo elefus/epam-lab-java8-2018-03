e49cfcfdc32af700e3c8868de0f2132e81b77296
Nikita Stepochkin <nikstepbox@gmail.com>
2018-03-24 02:05:26 +0300

    lambda.part3.exercise.Exercise1 remove stream

--- src/test/java/lambda/part3/exercise/Exercise1.java
+++ src/test/java/lambda/part3/exercise/Exercise1.java
@@ -5,6 +5,7 @@ import lambda.data.Person;
 import lambda.part3.example.Example1;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Function;
@@ -18,13 +19,14 @@ public class Exercise1 {
     @Test
     public void mapEmployeesToLengthOfTheirFullNames() {
         List<Employee> employees = Example1.getEmployees();
-        List<Integer> lengths;
+        List<Integer> lengths = new ArrayList<>();
 
         Function<Employee, Person> personExtractor = Employee::getPerson;
         Function<Person, String> fullNameExtractor = Person::getFullName;
         Function<String, Integer> stringLengthExtractor = String::length;
         Function<Employee, Integer> fullNameLengthExtractor = personExtractor.andThen(fullNameExtractor).andThen(stringLengthExtractor);
-        lengths = employees.stream().map(fullNameLengthExtractor).collect(Collectors.toList());
+
+        employees.forEach(em -> lengths.add(fullNameLengthExtractor.apply(em)));
 
         assertEquals(Arrays.asList(14, 19, 14, 15, 14, 16), lengths);
     }
