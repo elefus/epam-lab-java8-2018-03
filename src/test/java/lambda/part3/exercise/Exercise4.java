91978722d0e366e3714b3564ed1577828a708cf5
Nikita Stepochkin <nikstepbox@gmail.com>
2018-03-24 03:44:39 +0300

    lambda.part3.exercise.Exercise4 change type names

--- src/test/java/lambda/part3/exercise/Exercise4.java
+++ src/test/java/lambda/part3/exercise/Exercise4.java
@@ -6,7 +6,6 @@ import lambda.part3.example.Example1;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
@@ -17,12 +16,12 @@ import static org.junit.Assert.assertEquals;
 @SuppressWarnings({"unused", "ConstantConditions"})
 public class Exercise4 {
 
-    private static class LazyCollectionHelper<T1, T2> {
+    private static class LazyCollectionHelper<T, R> {
 
-        private final List<T1> source;
-        private final Function<T1, List<T2>> mappingFunction;
+        private final List<T> source;
+        private final Function<T, List<R>> mappingFunction;
 
-        public LazyCollectionHelper(List<T1> source, Function<T1, List<T2>> mappingFunction) {
+        public LazyCollectionHelper(List<T> source, Function<T, List<R>> mappingFunction) {
             this.source = source;
             this.mappingFunction = mappingFunction;
         }
@@ -31,21 +30,21 @@ public class Exercise4 {
             return new LazyCollectionHelper<>(list, Collections::singletonList);
         }
 
-        public <T3> LazyCollectionHelper<T1, T3> flatMap(Function<T2, List<T3>> flatMapping) {
+        public <U> LazyCollectionHelper<T, U> flatMap(Function<R, List<U>> flatMapping) {
             return new LazyCollectionHelper<>(source, mappingFunction.andThen(getListToListMappingFunction(flatMapping)));
         }
 
-        public <T3> LazyCollectionHelper<T1, T3> map(Function<T2, T3> mapping) {
+        public <U> LazyCollectionHelper<T, U> map(Function<R, U> mapping) {
             return new LazyCollectionHelper<>(source, mappingFunction.andThen(getListToListMappingFunction(mapping.andThen(Collections::singletonList))));
         }
 
-        public List<T2> force() {
+        public List<R> force() {
             return getListToListMappingFunction(mappingFunction).apply(source);
         }
 
-        private static <FROM, TO> Function<List<FROM>, List<TO>> getListToListMappingFunction(Function<FROM, List<TO>> mapping) {
+        private static <T1, T2> Function<List<T1>, List<T2>> getListToListMappingFunction(Function<T1, List<T2>> mapping) {
             return list -> {
-                List<TO> result = new ArrayList<>();
+                List<T2> result = new ArrayList<>();
                 list.forEach(mapping.andThen(result::addAll)::apply);
                 return result;
             };
