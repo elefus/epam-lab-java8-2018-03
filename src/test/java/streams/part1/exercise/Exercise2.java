bb16a451e167fc6775de86714cab9eef305fc842
Viacheslav Plekhanov <Viacheslav_PLekhanov@epam.com>
2018-03-26 14:56:26 +0300

    stream.part1.exercise.Exercise2(Plekhanov)

--- src/test/java/streams/part1/exercise/Exercise2.java
+++ src/test/java/streams/part1/exercise/Exercise2.java
@@ -1,11 +1,14 @@
 package streams.part1.exercise;
 
 import lambda.data.Employee;
+import lambda.data.JobHistoryEntry;
 import lambda.data.Person;
 import lambda.part3.example.Example1;
 import org.junit.Test;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
@@ -16,7 +19,9 @@ public class Exercise2 {
     public void calcAverageAgeOfEmployees() {
         List<Employee> employees = Example1.getEmployees();
 
-        Double expected = null;
+        Double expected = employees.stream()
+                .map(Employee::getPerson)
+                .mapToInt(Person::getAge).average().getAsDouble();
 
         assertEquals(33.66, expected, 0.1);
     }
@@ -25,7 +30,10 @@ public class Exercise2 {
     public void findPersonWithLongestFullName() {
         List<Employee> employees = Example1.getEmployees();
 
-        Person expected = null;
+        Person expected = employees.stream()
+                .map(Employee::getPerson)
+                .max(Comparator.comparingInt(person->person.getFullName().length()))
+                .orElse(null);
 
         assertEquals(expected, employees.get(1).getPerson());
     }
@@ -34,7 +42,9 @@ public class Exercise2 {
     public void findEmployeeWithMaximumDurationAtOnePosition() {
         List<Employee> employees = Example1.getEmployees();
 
-        Employee expected = null;
+        Employee expected = employees.stream()
+                .max(Comparator.comparingInt(employee-> employee.getJobHistory().stream()
+                        .mapToInt(JobHistoryEntry::getDuration).max().getAsInt())).orElse(null);
 
         assertEquals(expected, employees.get(4));
     }
@@ -48,7 +58,12 @@ public class Exercise2 {
     public void calcTotalSalaryWithCoefficientWorkExperience() {
         List<Employee> employees = Example1.getEmployees();
 
-        Double expected = null;
+        int baseSalary = 75000;
+        Double expected = employees.stream()
+                .map(Employee::getJobHistory)
+                .map(jobHistoryEntries -> jobHistoryEntries.get(jobHistoryEntries.size() - 1))
+                .mapToDouble(entry->entry.getDuration()>3?1.2*baseSalary:baseSalary)
+                .sum();
 
         assertEquals(465000.0, expected, 0.001);
     }
