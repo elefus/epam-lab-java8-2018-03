c39a8bfcb794d08e98d23e2326736ac97f795af6
Nikita Stepochkin <nikstepbox@gmail.com>
2018-03-28 18:54:45 +0300

    streams.part2.exercise.Exercise4

--- src/test/java/streams/part2/exercise/Exercise4.java
+++ src/test/java/streams/part2/exercise/Exercise4.java
@@ -3,8 +3,13 @@ package streams.part2.exercise;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.ToLongFunction;
 
+import static java.util.stream.Collectors.*;
 import static org.junit.Assert.assertEquals;
 
 public class Exercise4 {
@@ -18,7 +23,24 @@ public class Exercise4 {
      * @return Список отобранных слов (в нижнем регистре).
      */
     private List<String> getFrequentlyOccurringWords(String text, int numberWords) {
-        throw new UnsupportedOperationException();
+        return Arrays.stream(text.split(" "))
+                .map(String::toLowerCase)
+                .collect(
+                        collectingAndThen(
+                                groupingBy(
+                                        Function.identity(),
+                                        counting()
+                                ),
+                                stringLongMap -> stringLongMap.entrySet().stream()
+                                        .sorted(Comparator.comparingLong(
+                                                (ToLongFunction<Map.Entry<String, Long>>) Map.Entry::getValue)
+                                                .reversed()
+                                                .thenComparing(Map.Entry::getKey))
+                                        .map(Map.Entry::getKey)
+                                        .limit(numberWords)
+                                        .collect(toList())
+                        ));
+
     }
 
     @Test
