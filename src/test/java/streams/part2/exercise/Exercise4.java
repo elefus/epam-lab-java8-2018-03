c22cb91d999a88afd2315c0f7e7e9701aecf8b7a
mercenery@inbox.ru <mercenery@inbox.ru>
2018-03-30 06:22:42 +0300

    streams.part2.exercises.Exercise4.done(Fedulov)

--- src/test/java/streams/part2/exercise/Exercise4.java
+++ src/test/java/streams/part2/exercise/Exercise4.java
@@ -3,22 +3,42 @@ package streams.part2.exercise;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.ToIntFunction;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
 
 public class Exercise4 {
 
     /**
-     * Выбирает из текста наиболее часто встречающиеся слова.
-     * Подсчет слов выполняется без учета их регистра, т.е. "Привет", "привет", "пРиВеТ" - одно и то же слово.
-     * Если некоторые слова имеют одинаковую частоту, то в выходном списке они упорядочиваются в лексикографическом порядке.
-     * @param text Исходный текст в котором слова (в смешанном регистре) разделены пробелами.
+     * Выбирает из текста наиболее часто встречающиеся слова. Подсчет слов выполняется без учета их регистра, т.е.
+     * "Привет", "привет", "пРиВеТ" - одно и то же слово. Если некоторые слова имеют одинаковую частоту, то в выходном
+     * списке они упорядочиваются в лексикографическом порядке.
+     *
+     * @param text        Исходный текст в котором слова (в смешанном регистре) разделены пробелами.
      * @param numberWords Количество наиболее часто встречающихся слов, которые необходимо отобрать.
      * @return Список отобранных слов (в нижнем регистре).
      */
     private List<String> getFrequentlyOccurringWords(String text, int numberWords) {
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
+        return Stream.of(text.split(" "))
+                     .map(String::toLowerCase)
+                     .collect(Collectors.toMap(Function.identity(),
+                                               word -> 1,
+                                               (indexFirst, indexSecond) -> indexFirst + indexSecond))
+                     .entrySet()
+                     .stream()
+                     .sorted(Comparator.comparingInt((ToIntFunction<Map.Entry<String, Integer>>) Map.Entry::getValue)
+                                       .reversed()
+                                       .thenComparing(Map.Entry::getKey))
+                     .map(Map.Entry::getKey)
+                     .limit(numberWords)
+                     .collect(Collectors.toList());
     }
 
     @Test
@@ -32,14 +52,25 @@ public class Exercise4 {
 
     @Test
     public void test2() {
-        String source = "Lorem ipsum dolor sit amet consectetur adipiscing elit Sed sodales consectetur purus at "
-                      + "faucibus Donec mi quam tempor vel ipsum non faucibus suscipit massa Morbi lacinia velit "
-                      + "blandit tincidunt efficitur Vestibulum eget metus imperdiet sapien laoreet faucibus Nunc "
-                      + "eget vehicula mauris ac auctor lorem Lorem ipsum dolor sit amet consectetur adipiscing elit "
-                      + "Integer vel odio nec mi tempor dignissim";
+        String
+                source =
+                "Lorem ipsum dolor sit amet consectetur adipiscing elit Sed sodales consectetur purus at " +
+                "faucibus Donec mi quam tempor vel ipsum non faucibus suscipit massa Morbi lacinia velit " +
+                "blandit tincidunt efficitur Vestibulum eget metus imperdiet sapien laoreet faucibus Nunc " +
+                "eget vehicula mauris ac auctor lorem Lorem ipsum dolor sit amet consectetur adipiscing elit " +
+                "Integer vel odio nec mi tempor dignissim";
 
         List<String> result = getFrequentlyOccurringWords(source, 10);
 
-        assertEquals(Arrays.asList("consectetur", "faucibus", "ipsum", "lorem", "adipiscing", "amet", "dolor", "eget", "elit", "mi"), result);
+        assertEquals(Arrays.asList("consectetur",
+                                   "faucibus",
+                                   "ipsum",
+                                   "lorem",
+                                   "adipiscing",
+                                   "amet",
+                                   "dolor",
+                                   "eget",
+                                   "elit",
+                                   "mi"), result);
     }
 }
