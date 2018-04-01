85a21b1b4dd781a8574b8862a08ce5d872bdb72a
Viacheslav Plekhanov <Viacheslav_Plekhanov@epam.com>
2018-04-01 10:42:30 +0300

    streams.part2.exercise.Exercise2(Plekhanov)

--- src/test/java/streams/part2/exercise/Exercise2.java
+++ src/test/java/streams/part2/exercise/Exercise2.java
@@ -1,17 +1,24 @@
 package streams.part2.exercise;
 
+import javafx.util.Pair;
 import lambda.data.Employee;
+import lambda.data.JobHistoryEntry;
 import lambda.data.Person;
 import lambda.part3.example.Example1;
 import org.junit.Test;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
 @SuppressWarnings("ConstantConditions")
 public class Exercise2 {
 
+    private static <T> Set<T> mergeSets(Set<T> set1, Set<T> set2){
+        set1.addAll(set2);
+        return set1;
+    }
     /**
      * Преобразовать список сотрудников в отображение [компания -> множество людей, когда-либо работавших в этой компании].
      *
@@ -68,7 +75,18 @@ public class Exercise2 {
     public void employersStuffList() {
         List<Employee> employees = Example1.getEmployees();
 
-        Map<String, Set<Person>> result = null;
+        Map<String, Set<Person>> result =
+                employees.stream()
+                        .map(employee -> employee.getJobHistory().stream()
+                                .collect(Collectors.groupingBy(JobHistoryEntry::getEmployer,
+                                        Collectors.mapping(
+                                                (val)-> employee.getPerson(),
+                                                Collectors.toSet()))))
+                        .map(Map::entrySet)
+                        .flatMap(Set::stream)
+                        .collect(Collectors.toMap(Map.Entry::getKey,
+                                Map.Entry::getValue,
+                                Exercise2::mergeSets));
 
         Map<String, Set<Person>> expected = new HashMap<>();
         expected.put("yandex", new HashSet<>(Collections.singletonList(employees.get(2).getPerson())));
@@ -142,7 +160,10 @@ public class Exercise2 {
     public void indexByFirstEmployer() {
         List<Employee> employees = Example1.getEmployees();
 
-        Map<String, Set<Person>> result = null;
+        Map<String, Set<Person>> result = employees.stream()
+                        .collect(Collectors.toMap(employee-> employee.getJobHistory().get(0).getEmployer(),
+                                employee-> new HashSet<>(Collections.singletonList(employee.getPerson())),
+                                Exercise2::mergeSets));
 
         Map<String, Set<Person>> expected = new HashMap<>();
         expected.put("yandex", new HashSet<>(Collections.singletonList(employees.get(2).getPerson())));
@@ -166,7 +187,23 @@ public class Exercise2 {
     public void greatestExperiencePerEmployer() {
         List<Employee> employees = Example1.getEmployees();
 
-        Map<String, Person> collect = null;
+        Map<String, Person> collect = employees.stream()
+                .map(employee -> employee.getJobHistory().stream()
+                        .collect(Collectors.toMap(JobHistoryEntry::getEmployer,
+                                jobHistoryEntry -> new Pair<>(employee.getPerson(),
+                                        jobHistoryEntry.getDuration()),
+                                (personIntegerPair, personIntegerPair2) -> new Pair<>(personIntegerPair.getKey(),
+                                        personIntegerPair.getValue()+ personIntegerPair2.getValue()))))
+                .map(Map::entrySet)
+                .flatMap(Set::stream)
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (personIntegerPair, personIntegerPair2) -> personIntegerPair.getValue()>personIntegerPair2.getValue()
+                                ? personIntegerPair
+                                : personIntegerPair2))
+                .entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getKey,
+                        stringPairEntry -> stringPairEntry.getValue().getKey()));
 
         Map<String, Person> expected = new HashMap<>();
         expected.put("EPAM", employees.get(4).getPerson());
