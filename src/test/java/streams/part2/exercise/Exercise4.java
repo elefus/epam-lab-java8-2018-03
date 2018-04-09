bd76921f117d7269e0b92d408aa7bddbd6b74730
Andrei <Andrei_Bogus@epam.com>
2018-04-09 12:03:47 +0300

    done

--- src/test/java/streams/part2/exercise/Exercise4.java
+++ src/test/java/streams/part2/exercise/Exercise4.java
@@ -3,8 +3,16 @@ package streams.part2.exercise;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
+import static java.util.Comparator.reverseOrder;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
 import static org.junit.Assert.assertEquals;
 
 public class Exercise4 {
@@ -18,7 +26,18 @@ public class Exercise4 {
      * @return Список отобранных слов (в нижнем регистре).
      */
     private List<String> getFrequentlyOccurringWords(String text, int numberWords) {
-        throw new UnsupportedOperationException();
+
+        Comparator <Map.Entry<String,Long>> comparator = Map.Entry.comparingByValue(reverseOrder());
+
+       return Stream.of(text.split(" "))
+                    .map(String::toLowerCase)
+                    .collect(groupingBy(Function.identity(),counting()))
+                    .entrySet()
+                    .stream()
+                    .sorted(comparator.thenComparing(Map.Entry.comparingByKey()))
+                    .map(Map.Entry::getKey)
+                    .limit(numberWords)
+                    .collect(toList());
     }
 
     @Test
